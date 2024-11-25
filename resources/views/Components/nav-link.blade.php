255f7c03c4b031be9690d687ed1d362f454b8e9c
unknown <ululazmi4bs@gmail.com>
2024-11-25 18:37:29 +0700

    feat: View data and Route Wildcards

--- resources/views/Components/nav-link.blade.php
+++ resources/views/Components/nav-link.blade.php
@@ -1,5 +1,12 @@
-@props(['active' => false])
+@props(['active' => false, 'type' => 'desktop'])
 
-<a class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium"
-    aria-current="{{ $active ? 'page' : 'false' }}" {{ $attributes }}>{{ $slot }}
-</a>
+@if ($type === 'desktop')
+    <a class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium"
+        aria-current="{{ $active ? 'page' : 'false' }}" {{ $attributes }}>{{ $slot }}
+    </a>
+@elseif ($type === 'mobile')
+    <a class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} block rounded-md px-3 py-2 text-base font-medium"
+        aria-current="{{ $active ? 'page' : 'false' }}" {{ $attributes }}>{{ $slot }}</a>
+@else
+    <p>Unintended error</p>
+@endif
