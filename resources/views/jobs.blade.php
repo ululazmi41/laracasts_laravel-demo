96802564f2b254522596fbe0e8af7f1f65ad9b2c
unknown <ululazmi4bs@gmail.com>
2024-11-26 16:18:30 +0700

    feat: N+1 problem

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -3,11 +3,16 @@
         Jobs Page
     </x-slot:heading>
 
-    @foreach ($jobs as $job)
-        <li>
-            <a href="/jobs/{{ $job['id'] }}" class="text-blue-500 hover:underline">
-                <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year.
+    <div class="space-y-4">
+        @foreach ($jobs as $job)
+            <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded-lg">
+                <div class="font-bold text-blue-500 text-sm">
+                    {{ $job->employer->name }}
+                </div>
+                <div>
+                    <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year.
+                </div>
             </a>
-        </li>
-    @endforeach
+        @endforeach
+    </div>
 </x-layout>
