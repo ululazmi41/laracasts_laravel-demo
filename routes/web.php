6a7547b8b7ccd0c2ce52126bf86b5990fb8f577e
unknown <ululazmi4bs@gmail.com>
2024-11-26 18:18:55 +0700

    feat: forms and CSRF

--- routes/web.php
+++ routes/web.php
@@ -8,15 +8,29 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    return view('jobs', [
-        'jobs' => Job::with('employer')->simplePaginate(3),
+    return view('jobs.index', [
+        'jobs' => Job::with('employer')->latest()->simplePaginate(3),
     ]);
+});
+
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
 });
 
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
 
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
+});
+
+Route::post('/jobs', function () {
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1
+    ]);
+
+    return redirect('/jobs');
 });
 
 Route::get('/contact', function () {
